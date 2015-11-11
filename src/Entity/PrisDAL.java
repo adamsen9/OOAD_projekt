46b74be388ff46329e16eab9e7a1ff1f04f305cd
Mikkel Westh <westh_mikkel@hotmail.com>
2015-11-11 01:15:29 +0100

    Admin Stuff

---
+++ src/Entity/PrisDAL.java
@@ -0,0 +1,13 @@
+package Entity;
+
+import java.util.ArrayList;
+import Entity.Dataklasser.Prinsinformation;
+
+public class PrisDAL {
+
+	public static ArrayList<Prinsinformation> getPriser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/Entity/SæsonDAL.java
@@ -0,0 +1,12 @@
+package Entity;
+
+import java.util.ArrayList;
+import Entity.Dataklasser.Sæson;
+
+public class SæsonDAL {
+	public static ArrayList<Sæson> getSæsoner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
